1dabfda501777bb88d838e0403a541912c1cc08a
Sthefani Denhof <sdenhof7@gmail.com>
2025-04-15 17:37:38 -0700

    Slicing

--- DSA.py
+++ DSA.py
@@ -792,13 +792,121 @@ my_string = "Hello"
 
 for i in range(len(my_string)):
     print(i, my_string[i])     # para ter o numero tbm printed, apenas adicionar o "i," 
-
+print("-----")
 
 # another ex:
 
 
 def print_string_characters(word1: str, word2:str) -> None:
-    pass
+    for c in word1:
+        print(c)
+    for c in word2:
+        print(c)
 
 
 print_string_characters("Hello,World!","Good Job!")
+print("-----")
+
+
+# ---------- STRING CONCATENATION --------------
+# CONCATINATION IS A PROCESS OS COMBINING TWO OR MORE STRINGS into a single using "+"
+
+def concatenate(s1: str, s2: str) -> str:
+    s3 = s1 + s2
+    if len(s3) > 10:
+        return "Too long!"
+    return s3
+
+
+print(concatenate("He", "llo"))
+print(concatenate("Hello", "world!"))
+print(concatenate("Length", "of10"))
+print("-----")
+
+
+# ----------- STRING SLICING PART 1 ----------------------------
+# If we only want to access a portuon of a string we can slicing. Slicing allows us to extract a substring frm a string by soecifying a range of indices.
+
+my_string = "Hello, world!"
+
+start, end = 1,5
+
+print(my_string[start:end]) # output: ello
+print("-----")
+
+#another ex:
+
+def get_substring(input_string:str, start: int, end:int) -> str:
+    if end > len(input_string):
+        return ""
+    
+    return input_string[start:end]
+
+print(get_substring("SeattLe", 1,7))
+print(get_substring("SeattLe", 1,8))
+print(get_substring("SeattLe", 1,9))
+print(get_substring("SeattLe", 0,2))
+print(get_substring("SeattLe", 0,7))
+print("-----")
+
+# ----------------------- STRING SLICING PART 2 ----------------------------
+# if we dont specify the start, it's equivalent to starting from the beginning of the string
+
+my_string = "Seattle"
+
+print(my_string[:3])   #output: Sea
+print(my_string[0:3])  #output: Sea
+print("-----")
+#if we don't specify the end, it's equivalent to ending at the end of the string
+
+my_string = "Seattle"
+
+print(my_string[3:])   #output: Sea
+print(my_string[3:7])  #output: Sea
+print("-----")
+
+#another ex:
+
+def first_n_characters(s:str, n:int) -> str :
+    return s[:n]
+
+def last_n_characters(s:str, n:int) -> str:
+    index = len(s) - n
+    return s[index:]
+
+print(first_n_characters("Seattle", 3))
+print(first_n_characters("Seattle", 4))
+print(first_n_characters("Seattle", 7))
+print("-----")
+print(last_n_characters("Seattle", 3))
+print(last_n_characters("Seattle", 4))
+print(last_n_characters("Seattle", 7))
+print("-----")
+
+
+# ----------------------- REVERSING A STRING ----------------------------
+# We can also slicing to reverse a string. By not specifying the starting of the index or the ending index, and setting the step to -1, the string will be reversed.
+
+my_string = "Hello"
+
+print(my_string[::-1]) # output: olleH
+print("-----")
+
+#another ex:
+
+my_string = "Hello"
+start, end, step = 1, 4, 1
+
+#print(my_string[start:end])       # output: ello
+
+print(my_string[start:end:-1])   # output: lle
+
+#another ex:
+
+def reverse_string(input_string:str) -> str:
+    return input_string[::-1]
+
+print(reverse_string("Seattle"))   # output: elttaeS
+print(reverse_string("Sthefani"))  # output: inafehtS
+print(reverse_string("Hello"))     # output: olleH
+print("-----")
