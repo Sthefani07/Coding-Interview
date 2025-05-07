deb026c71e394dd3d233f7480101f8c1c1abc31b
Sthefani Denhof <sdenhof7@gmail.com>
2025-05-06 17:22:20 -0700

    List.pop()

--- DSA.py
+++ DSA.py
@@ -1193,3 +1193,50 @@ def append_to_list(my_list: List[int], elements: List[int]) -> List[int]:
 print(append_to_list([1, 2, 3], [4, 5])) #Output: [1, 2, 3, 4, 5]
 print(append_to_list([], [1, 2, 3, 4]))  #Output: [1, 2, 3, 4]
 print("-----")
+
+
+# ----------------------- LIST POP ----------------------------
+
+# We can also remove elements from a list using the pop() function.
+
+my_list = [1, 2, 3]
+
+my_list.pop()
+print(my_list) #OUtput: [1, 2]
+print("-----")
+
+# We can also specify an index to remove a specific element, as shown below
+
+my_list = [1, 2, 3]
+my_list.pop(0)
+print(my_list) #Output: [2, 3]
+
+my_list.pop(1)
+print(my_list)  #Output: [2]
+print("-----")
+
+#another ex:
+
+from typing import List
+
+def remove_from_list(my_list: List[int], index: int) -> List[int]:
+    my_list.pop(index)
+    return my_list
+
+
+
+def pop_n_from_list(my_list: List[int], n:int) -> List[int]:
+    while n > 0:
+        my_list.pop()
+        n = n-1
+    return my_list
+
+
+print(remove_from_list([1, 2, 3, 4, 5], 2))
+print(remove_from_list([1, 2, 3, 4, 5], 0))
+print(remove_from_list([1, 2, 3, 4, 5], 4))
+
+
+print(pop_n_from_list([1, 2, 3, 4, 5], 2))
+print(pop_n_from_list([1, 2, 3, 4, 5], 0))
+
